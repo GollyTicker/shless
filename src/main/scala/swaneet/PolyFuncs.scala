af5c3489a2dccab3780c898c519c38368784db97
GollyTicker <golly.ticker@gmail.com>
2014-06-01 10:30:25 +0200

    poly funcs finished with example

--- src/main/scala/swaneet/PolyFuncs.scala
+++ src/main/scala/swaneet/PolyFuncs.scala
@@ -8,34 +8,47 @@ import shapeless.poly._
  */
 object PolyFuncs {
 
-  object ident extends (Id ~> Id) {
-    def apply[A](x:A) = x
-  }
+  // Viele Anwendungen.
+
+  // hier nur ein kleines Beispiel wie man einfacher und verständlicher type-class artige Funktionen implementieren kann
+
 
   object reverse extends Poly1 {
+
     import Integer.parseInt
 
-    implicit def revInt               = at[Int]( x => parseInt(x.toString().reverse) )
+    // reverse für Integers
+    implicit def revInt = at[Int](x => parseInt(x.toString().reverse))
 
-    implicit def revString            = at[String](_.reverse)
+    // reverse für Strings
+    implicit def revString = at[String](_.reverse)
 
-    implicit def revList[A](implicit revA:Case.Aux[A,A])
-                                    = at[List[A]](ls => (ls map(reverse(_)) reverse))
+    // reverse für Listen
+    implicit def revList[A](implicit revA: Given[A, A])
+    = at[List[A]](ls => (ls map (reverse(_)) reverse))
 
-    implicit def revTuple[A, B](implicit revA : Case.Aux[A, A], revB : Case.Aux[B, B])
-                                    = at[(A, B)]{ case (a, b) => (reverse(b), reverse(a))}
+    // reverse für Tupeln
+    implicit def revTuple[A, B](implicit revA: Given[A, A], revB: Given[B, B])
+    = at[(A, B)]{ case (a, b) => (reverse(b), reverse(a)) }
+
+    type Given[A, B] = Case.Aux[A, B]
   }
 
   def apply() = {
 
     println("strLen Int: " + reverse(324))
     println("strLen String: " + reverse("12345"))
-    println("strLen List: " + reverse(List(2,3,5,63)))
-    println("strLen List: " + reverse(List("2","3","5","63")))
-    println("strLen List: " + reverse(("abedc",123)))
+    println("strLen List: " + reverse(List(2, 3, 5, 63)))
+    println("strLen List: " + reverse(List("2", "3", "5", "63")))
+    println("strLen List: " + reverse(("abedc", 123)))
 
-    println("strLen List: " + ((14 :: 23 :: "sdfsdf" :: HNil) map reverse ) )
+    println("strLen List: " + ((14 :: 23 :: "sdfsdf" :: HNil) map reverse))
+
+    import shapeless.test.illTyped
+    illTyped("reverse(true)") // compiliert nicht, weil kein reverse über Booleans definiert ist.
+
+    illTyped("reverse(Set(1,2,4))") // compiliert nicht, weil kein reverse über Sets definiert ist.
+
   }
-
 
 }
